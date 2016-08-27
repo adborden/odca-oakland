9d7411af62b65f741fd79de95cc9bd3cc221dde5
Aaron D Borden <adborden@a14n.net>
2016-08-27 13:33:25 -0700

    Home page content

--- src/index.js
+++ src/index.js
@@ -7,27 +7,13 @@
 'use strict';
 
 var angular = require('angular');
-var GoogleSpreadsheet = require('./gsheet');
 
 
 angular.module('odca', [])
-  .run(function ($rootScope) {
-    $rootScope.text = 'hello world';
-    $rootScope.foo = { text: 'hi hi'};
+  .config(function ($locationProvider) {
+    $locationProvider.html5Mode(false).hashPrefix('!');
   })
-  .controller('home', function ($scope) {
-    $scope.candidates = [];
-
-    var sheet = new GoogleSpreadsheet('1272oaLyQhKwQa6RicA5tBso6wFruum-mgrNm3O3VogI');
-    sheet.init(function () {
-      sheet.getHeaders()
-        .then(function () {
-          sheet.getColumn('Candidate').then(function (values) {
-            Array.prototype.splice.apply($scope.candidates, [0, $scope.candidates.length].concat(values));
-            $scope.$digest();
-          });
-        });
-    });
+  .controller('home', function () {
   })
   .controller('candidates', require('./candidates').controller)
   .controller('committees', require('./committees').controller);
