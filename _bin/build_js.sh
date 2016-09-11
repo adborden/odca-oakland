0daeef3f08950d9a665caef9fc14b2e288230950
Aaron D Borden <adborden@a14n.net>
2016-09-11 12:29:03 -0700

    Production build with minification

--- _bin/build_js.sh
+++ _bin/build_js.sh
@@ -5,6 +5,12 @@ set -o pipefail
 set -x
 
 js_asset_dir=_site/assets/js
+uglify_extra_opts=
+
+if [[ "${1}" == "--prod" ]]; then
+  # Production build
+  uglify_extra_opts="--mangle"
+fi
 
 mkdir -p $js_asset_dir
 
@@ -22,10 +28,11 @@ for entrypoint in src/*.js; do
 
   # Having the base_url from jekyll complicates things. We have to be explicit
   # with all the paths and names and remove the prefix created from js_asset_dir.
-  uglifyjs --mangle --compress \
+  uglifyjs --compress \
     --prefix 3 \
     --in-source-map ${js_asset_dir}/${bundle_js_map} \
     --source-map-url ${bundle_min_map} \
     --source-map ${js_asset_dir}/${bundle_min_map} \
+    ${uglify_extra_opts} \
     -o ${js_asset_dir}/${bundle_min} -- ${js_asset_dir}/${bundle_js}
 done
