6e8caaf69658f5190180dec9b3c642b19b64a287
Aaron D Borden <adborden@a14n.net>
2016-09-16 00:52:02 -0700

    Update page title on route change

--- src/common/index.js
+++ src/common/index.js
@@ -5,6 +5,7 @@ var angular = require('angular');
 angular.module('common', [
   require('./assets.service'),
   require('./default.filter'),
+  require('./page_title.service'),
   require('./url.filter')
 ]);
 
